4884871a0f3f5009012576f2a702f85636686561
Caren_R <97258545+Caren302@users.noreply.github.com>
2022-04-05 03:30:15 +0000

    Funciona el punto 32 y el punto 44.

--- sokoban.py
+++ sokoban.py
@@ -21,10 +21,10 @@ class Mi_primer_juego:
   mapa =[
         [3,3,3,3,3,3,3,3,3,3,3,3,3,3],
         [3,1,1,1,1,1,1,1,1,1,1,1,1,3],
-        [3,1,1,1,1,4,1,1,1,1,0,1,1,3],
-        [3,1,1,1,1,1,1,1,1,1,2,1,1,3],
+        [3,1,1,1,1,4,1,1,1,1,1,1,1,3],
         [3,1,1,1,1,1,1,1,1,1,1,1,1,3],
-        [3,1,1,1,1,1,1,1,1,2,1,1,1,3],
+        [3,1,1,1,1,0,1,1,1,1,1,1,1,3],
+        [3,1,1,1,1,2,1,1,1,2,1,1,1,3],
         [3,1,1,1,1,1,1,1,1,1,1,1,1,3],
         [3,1,1,1,1,4,1,1,1,1,1,1,1,3],
         [3,3,3,3,3,3,3,3,3,3,3,3,3,3]
@@ -235,6 +235,13 @@ class Mi_primer_juego:
         self.mapa[self.muneco_fila-2,self.muneco_columna]=2
         self.muneco_fila-=1
 
+    #32 muneco,caja,meta [0,2,4] -> [1,0,6] 
+    elif self.mapa[self.muneco_fila,self.muneco_columna]== 0 and  self.mapa[self.muneco_fila-1,self.muneco_columna]==2 and self.mapa[self.muneco_fila-2,self.muneco_columna]==4:
+        self.mapa[self.muneco_fila,self.muneco_columna]=1
+        self.mapa[self.muneco_fila-1,self.muneco_columna]=0
+        self.mapa[self.muneco_fila-2,self.muneco_columna]=6
+        self.muneco_fila-=1
+
   def moverAbajo(self):
     #41 muneco,espacio [1,0] -> [0,1]
     if self.mapa[self.muneco_fila,self.muneco_columna]== 0 and  self.mapa[self.muneco_fila+1,self.muneco_columna]==1:
@@ -248,11 +255,18 @@ class Mi_primer_juego:
         self.mapa[self.muneco_fila+1,self.muneco_columna]=5
         self.muneco_fila+=1
 
-    #42 muneco,caja,espacio [0,2,1] -> [1,0,2] 
+    #43 muneco,caja,espacio [0,2,1] -> [1,0,2] 
     elif self.mapa[self.muneco_fila,self.muneco_columna]== 0 and  self.mapa[self.muneco_fila+1,self.muneco_columna]==2 and self.mapa[self.muneco_fila+2,self.muneco_columna]==1 :
         self.mapa[self.muneco_fila,self.muneco_columna]=1
         self.mapa[self.muneco_fila+1,self.muneco_columna]=0
         self.mapa[self.muneco_fila+2,self.muneco_columna]=2
+        self.muneco_fila+=1
+
+    #44 muneco,caja,meta [0,2,4] -> [1,0,6] 
+    elif self.mapa[self.muneco_fila,self.muneco_columna]== 0 and  self.mapa[self.muneco_fila+1,self.muneco_columna]==2 and self.mapa[self.muneco_fila+2,self.muneco_columna]==4:
+        self.mapa[self.muneco_fila,self.muneco_columna]=1
+        self.mapa[self.muneco_fila+1,self.muneco_columna]=0
+        self.mapa[self.muneco_fila+2,self.muneco_columna]=6
         self.muneco_fila+=1
                    
 juego = Mi_primer_juego()#Crea un objeto para jugar
